b38ed6c2d53ebabd7e58ebda713aeea1151e4229
Nao Minami <south37777@gmail.com>
2015-10-24 16:56:04 +0900

    Parse binary and octhedral and hexagonal number

--- simple-parser.hs
+++ simple-parser.hs
@@ -1,6 +1,8 @@
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
 import Control.Monad
+import Data.Char
+import Numeric
 
 symbol :: Parser Char
 symbol = oneOf  "!#$%&|*+-/:<=>?@^_~"
@@ -39,8 +41,22 @@ parseAtom = do first <- letter <|> symbol
                           _    -> Atom atom
 
 parseNumber :: Parser LispVal
-parseNumber = do number <- many1 digit
-                 return $ Number (read number)
+parseNumber = do prefix <- ((char '#' >> oneOf "box") <|> digit)
+                 case prefix of
+                   'b' -> do numberStr <- many1 $ oneOf "01"
+                             return $ Number (parseBin numberStr)
+                   'o' -> do numberStr <- many1 $ oneOf "01234567"
+                             return $ Number (extractNumber $ readOct numberStr)
+                   'x' -> do numberStr <- many1 $ oneOf "0123456789abcde"
+                             return $ Number (extractNumber $ readHex numberStr)
+                   _   -> do numberStr <- many1 digit
+                             return $ Number (read $ prefix:numberStr)
+
+extractNumber :: [(a, String)] -> a
+extractNumber ((number, _):_) = number
+
+parseBin :: String -> Integer
+parseBin input = foldl (\x y -> x * 2 + y) 0 $ map (\x -> read [x]) input
 
 parseExpr :: Parser LispVal
 parseExpr = parseAtom
