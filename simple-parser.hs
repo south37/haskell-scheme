dbf992162cf66636fef73842d29d9f78000a9356
Nao Minami <south37777@gmail.com>
2015-10-25 19:38:36 +0900

    Add if

--- simple-parser.hs
+++ simple-parser.hs
@@ -243,6 +243,11 @@ eval val@(Number _) = return val
 eval val@(Character _) = return val
 eval val@(Bool _) = return val
 eval (List [Atom "quote", val]) = return val
+eval (List [Atom "if", pred, conseq, alt]) =
+    do result <- eval pred
+       case result of
+         Bool False -> eval alt
+         otherwize -> eval conseq
 eval (List (Atom func : args)) = mapM eval args >>= apply func
 eval badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
 
