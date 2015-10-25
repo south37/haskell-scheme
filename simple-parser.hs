62bd6df7d3d30e8cc8e0a62fdb0e17b2554b2503
Nao Minami <south37777@gmail.com>
2015-10-25 14:34:36 +0900

    Add LispError

--- simple-parser.hs
+++ simple-parser.hs
@@ -3,6 +3,26 @@ import System.Environment
 import Control.Monad
 import Data.Char
 import Numeric
+import Control.Monad.Error
+
+data LispError = NumArgs Integer [LispVal]
+               | TypeMismatch String LispVal
+               | Parser ParseError
+               | BadSpecialForm String LispVal
+               | NotFunction String String
+               | UnboundVar String String
+               | Default String
+
+showError :: LispError -> String
+showError (UnboundVar message varname) = message ++ ": " ++ varname
+showError (BadSpecialForm message form) = message ++ ": " ++ show form
+showError (NotFunction message func) = message ++ ": " ++ show func
+showError (NumArgs expected found) = "Expected " ++ show expected
+                                  ++ " args; found values " ++ unwordsList found
+showError (TypeMismatch expected found) = "Invalid type: expected " ++ expected
+                                       ++ ", found " ++ show found
+showError (Parser parseError) = "Parse error at " ++ show parseError
+instance Show LispError where show = showError
 
 escapedChars :: Parser Char
 escapedChars = do x <- char '\\' >> (oneOf "nrt\\\"")
