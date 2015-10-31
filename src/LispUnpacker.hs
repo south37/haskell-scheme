e210a44db42e3f8ed58aa85ebbbad43b27c5e52d
Nao Minami <south37777@gmail.com>
2015-10-31 17:52:32 +0900

    Divide to LispInterpreter and LispUnpacker

---
+++ src/LispUnpacker.hs
@@ -0,0 +1,18 @@
+module LispUnpacker
+( Unpacker ( AnyUnpacker )
+, unpackEquals
+) where
+
+import Control.Monad.Error
+import LispVal
+import LispError
+
+data Unpacker = forall a. Eq a => AnyUnpacker (LispVal -> ThrowsError a)
+
+unpackEquals :: LispVal -> LispVal -> Unpacker -> ThrowsError Bool
+unpackEquals arg1 arg2 (AnyUnpacker unpacker) =
+             do unpacked1 <- unpacker arg1
+                unpacked2 <- unpacker arg2
+                return $ unpacked1 == unpacked2
+         `catchError` (const $ return False)
+
