0bdd1385f26b02a05053104ed8418269d0e6f8e5
Nao Minami <south37777@gmail.com>
2015-11-01 00:09:16 +0900

    Refactor LispError

--- src/LispInterpreter/LispError.hs
+++ src/LispInterpreter/LispError.hs
@@ -35,8 +35,9 @@ showError :: LispError -> String
 showError (UnboundVar message varname) = message ++ ": " ++ varname
 showError (BadSpecialForm message form) = message ++ ": " ++ show form
 showError (NotFunction message func) = message ++ ": " ++ show func
-showError (NumArgs expected found) = "Expected " ++ show expected
-                                  ++ " args; found values " ++ LispVal.unwordsList found
+showError (NumArgs expected found) =
+    "Expected " ++ show expected
+ ++ " args; found values " ++ LispVal.unwordsList found
 showError (TypeMismatch expected found) = "Invalid type: expected " ++ expected
                                        ++ ", found " ++ show found
 showError (Parser parseError) = "Parse error at " ++ show parseError
