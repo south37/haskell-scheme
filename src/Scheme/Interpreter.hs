d0dd420df1a5e3ac611cd5dd43a74c12997000f5
Nao Minami <south37777@gmail.com>
2015-11-08 09:57:25 +0900

    Add Scheme/Interpreter

---
+++ src/Scheme/Interpreter.hs
@@ -0,0 +1,47 @@
+module Scheme.Interpreter
+( evalAndPrint
+, runRepl
+) where
+
+import qualified Text.ParserCombinators.Parsec as Parsec
+import qualified System.IO as IO
+import qualified Control.Monad as Monad
+import qualified Control.Monad.Error as Error
+
+import           Scheme.LispVal (LispVal)
+import qualified Scheme.Parser as Parser
+import qualified Scheme.Evaluator as Evaluator
+import qualified Scheme.LispError as LispError
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+evalString :: String -> IO String
+evalString expr =
+    let evaled = readExpr expr >>= Evaluator.eval
+    in return $ extractValue $ LispError.trapError (Monad.liftM show evaled)
+
+extractValue :: LispError.ThrowsError a -> a
+extractValue (Right val) = val
+
+flushStr :: String -> IO ()
+flushStr str = IO.putStr str >> IO.hFlush IO.stdout
+
+readExpr :: String -> LispError.ThrowsError LispVal
+readExpr input = case Parsec.parse Parser.parseExpr "lisp" input of
+    Left err -> Error.throwError $ LispError.Parser err
+    Right val -> return val
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Lisp>>> ") evalAndPrint
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do
+    result <- prompt
+    if pred result
+        then return ()
+        else action result >> until_ pred prompt action
+
